7d5a390c0d146f0e75274a83e7b6a74eb6c1031b
seiyria <kyle@seiyria.com>
2016-12-26 15:19:06 -0600

    base report layout done

--- src/client/pages/reporting/reporting.ts
+++ src/client/pages/reporting/reporting.ts
@@ -1,3 +1,5 @@
+import * as _ from 'lodash';
+
 import { Component } from '@angular/core';
 
 type ReportRoute =
@@ -34,6 +36,11 @@ const invoiceColumns = [
 class ReportConfiguration {
   name: string;
   reportRoute: ReportRoute;
+  hasDateFilter?: boolean;
+  singleDateFilter?: boolean;
+  startDate?: Date;
+  endDate?: Date;
+  dateText?: string = 'Date Range';
   columnOrder?: string[];
   columnChecked?: string[];
   columns: any[];
@@ -45,23 +52,38 @@ class ReportConfiguration {
 })
 export class ReportingPageComponent {
 
+  currentReport: ReportConfiguration;
+
   baseReports: ReportConfiguration[] = [
     { name: 'Inventory (Current)', reportRoute: 'base/inventory/current', columns: stockItemColumns,
       columnChecked: ['Name', 'SKU', 'OU', 'Cost', 'Quantity' ] },
     { name: 'Inventory (Old)',     reportRoute: 'base/inventory/old',     columns: stockItemColumns,
+      hasDateFilter: true, singleDateFilter: true, dateText: 'Items Older Than',
       columnChecked: ['Name', 'SKU', 'OU', 'Cost', 'Quantity' ] },
     { name: 'Inventory (Reorder)', reportRoute: 'base/inventory/reorder', columns: stockItemColumns,
       columnChecked: ['Name', 'Quantity', 'Vendor Name', 'Vendor SKU', 'Vendor Cost'] },
     { name: 'Sales (Completed)',   reportRoute: 'base/sales/completed',   columns: invoiceColumns,
+      hasDateFilter: true,
       columnChecked: ['Purchase Time', 'Purchase Method', 'Purchase Price', 'Tax Collected', 'Subtotal', '# Items'] },
     { name: 'Sales (Voided)',      reportRoute: 'base/sales/voided',      columns: invoiceColumns,
+      hasDateFilter: true,
       columnChecked: ['Purchase Time', 'Purchase Method', '# Items']  },
     { name: 'Sales (Tax)',         reportRoute: 'base/sales/tax',         columns: invoiceColumns,
+      hasDateFilter: true,
       columnChecked: ['Purchase Time', 'Tax Collected'] }
   ];
 
   selectNewReport(reportConfig: ReportConfiguration) {
+    this.currentReport = _.cloneDeep(reportConfig);
+    _.each(this.currentReport.columnChecked, col => {
+      _.find(this.currentReport.columns, { name: col }).checked = true;
+    });
+  }
 
+  reorderColumns({ from, to }) {
+    let element = this.currentReport.columns[from];
+    this.currentReport.columns.splice(from, 1);
+    this.currentReport.columns.splice(to, 0, element);
   }
 
   constructor() {
