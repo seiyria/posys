22b2a01f91f35679a1e459cba3f106b008d5d210
seiyria <kyle@seiyria.com>
2017-01-15 21:07:13 -0600

    lcoses #76

--- seeds/location.js
+++ seeds/location.js
@@ -12,8 +12,17 @@ const createLocation = (knex, ou) => {
 
 exports.seed = (knex) => {
   return knex('location')
-    // .del()
-    .then(() => {
-      return Promise.all(locations.map(location => createLocation(knex, location)));
+    .count()
+    .then(countArray => {
+      const count = countArray[0].count;
+      if(count > 0) {
+        console.log('[Seed] Skipping location, count(*) > 0.');
+        return;
+      }
+      return knex('location')
+      // .del()
+        .then(() => {
+          return Promise.all(locations.map(location => createLocation(knex, location)));
+        });
     });
 };
--- seeds/organizationalunit.js
+++ seeds/organizationalunit.js
@@ -12,8 +12,17 @@ const createOU = (knex, ou) => {
 
 exports.seed = (knex) => {
   return knex('organizationalunit')
-    // .del()
-    .then(() => {
-      return Promise.all(ous.map(ou => createOU(knex, ou)));
+    .count()
+    .then(countArray => {
+      const count = countArray[0].count;
+      if(count > 0) {
+        console.log('[Seed] Skipping organizationalunit, count(*) > 0.');
+        return;
+      }
+      return knex('organizationalunit')
+      // .del()
+        .then(() => {
+          return Promise.all(ous.map(ou => createOU(knex, ou)));
+        });
     });
 };
